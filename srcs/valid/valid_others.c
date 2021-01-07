1fd072791b818bda2ceab1fb1df3ef11b84299dc
Taisei Toya <tytaisei0102@gmail.com>
2021-01-07 12:18:11 +0100

    fix: valid_map

--- srcs/valid/valid_others.c
+++ srcs/valid/valid_others.c
@@ -6,7 +6,7 @@
 /*   By: totaisei <totaisei@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/04 13:41:40 by totaisei          #+#    #+#             */
-/*   Updated: 2021/01/07 10:14:21 by totaisei         ###   ########.fr       */
+/*   Updated: 2021/01/07 12:09:06 by totaisei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,6 @@ t_status	valid_new_line(t_game *game, t_ident_line input)
 	if (game->config.map_flag == TRUE && game->config.finish_map_flag == FALSE)
 	{
 		game->config.finish_map_flag = TRUE;
-		return (NOT_ENTERED);
 	}
-	return (FAILURE);
+	return (NOT_ENTERED);
 }
